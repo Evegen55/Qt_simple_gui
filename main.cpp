3aef9cfee14a3b44d9305424bfa47dcdde572cad
Evgenii Lartcev <i.dolende@gmail.com>
2019-01-27 05:07:57 +0300

    window and app title

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setApplicationName(a.translate("main", "System info"));
+    a.setOrganizationName("Evgen Ltd.");
+    a.setOrganizationDomain("evgen.eu");
+    a.setQuitOnLastWindowClosed(false);
+
     MainWindow w;
+    w.setWindowTitle(a.organizationName());
     w.show();    
 
     return a.exec();
